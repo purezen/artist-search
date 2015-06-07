dc3e6d42181481714c576f0c0b979a5d1f5c299b
Aditya Bhardwaj <aditya2204@gmail.com>
2015-06-07 14:17:34 +0530

    api working!

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,20 @@
 class SearchController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!,:lastfm_init
 
   def index
+    if params[:search]
+      @artists = @lastfm.artist.search(artist: params[:search])['results']['artistmatches']['artist']
+    end
   end
+
+  private
+
+    def lastfm_init
+      unless @lastfm
+        lastfm ||= Lastfm.new('b8a8da7b4164232fa03f4930f827112b', '6ce62a3647fb7a2c1422051d8b65eaee')
+        token ||= '95ff39af37f0c4b4e1fa0725ac8fe5fe'
+        lastfm.session ||= lastfm.auth.get_session(token: token)['key']
+        @lastfm = lastfm
+      end
+    end
 end
