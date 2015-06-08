fad828efcff590f8d86cd8c5d284bd51a86151e1
Aditya Bhardwaj <aditya2204@gmail.com>
2015-06-08 14:40:54 +0530

    setup search history model
    asdas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606142356) do
+ActiveRecord::Schema.define(version: 20150608090910) do
+
+  create_table "searches", force: true do |t|
+    t.string   "history"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
